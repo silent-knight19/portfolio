3852e1718204e1be7cb412482119388905c95934
silent-knight19 <sachinsinghtomar7749@gmail.com>
2025-07-23 15:14:14 +0530

    getting started

---
+++ my-portfolio/src/components/LandingPage.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const LandingPage = () => {
+    return (
+        <>
+            <div className="d1">
+                <div className="navbar" style={{ width: "100%" }}>
+                    <p>About Me</p>
+                    <p>Portfolio</p>
+                    <p>Services</p>
+                    <p>Contact</p>
+                </div>
+                <div className="d3" style={{ width: "50%" }}>
+                    <div className="d3.1"></div>
+                    <div className="d3.2"></div>
+                </div>
+                <div className="d4" style={{ width: "50%" }}></div>
+            </div>
+        </>
+    );
+};
+
+export default LandingPage;
